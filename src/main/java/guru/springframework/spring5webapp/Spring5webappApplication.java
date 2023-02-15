bda83fd63b503b9be08ab54bf60e0d550bd72973
Felix Schmid <felix.schmid@breuninger.de>
2023-02-15 16:00:44 +0100

    y

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//Herumspringen
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
